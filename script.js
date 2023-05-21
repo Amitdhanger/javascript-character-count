2b725c4cdce934499b571a2ba2e6ec2e598422f0
Amitdhanger <126848870+Amitdhanger@users.noreply.github.com>
2023-05-21 09:07:10 +0530

    javascript-character-count

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.querySelector('.text-box').addEventListener('keypress', function(){
+
+    let charactercount = document.querySelector('.text-box').value.length
+    let leftcharacters = 150- charactercount
+    document.querySelector('.number').innerText = leftcharacters
+})
